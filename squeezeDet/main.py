73a9feaa90e73cc00ddffa6d4ad3a6026b2b20fb
Olle Norelius <norelius.olle@gmail.com>
2017-04-06 12:47:44 +0200

    Moved batching to CPU and separated saving from eval

--- squeezeDet/main.py
+++ squeezeDet/main.py
@@ -6,12 +6,17 @@ import kitti_reader as kr
 import network as net
 
 with tf.variable_scope('Input_batching'):
-    data_folders=["data/set1", "data/set2", "data/set3"]
+    data_folders=["data/set1",
+                  "data/set2",
+                  "data/set3",
+                  "data/set4",
+                  "data/set6"]
     validation_folders=['data/validation']
 
-    batch = reader.get_batch(4,data_folders)
-    #batch = kr.get_batch(40,"/home/local-admin/KITTI")
-    validation_batch = reader.get_batch(29, validation_folders)
+    with tf.device('/cpu:0'):
+        batch = reader.get_batch(params.BATCH_SIZE,data_folders)
+        #batch = kr.get_batch(40,"/home/local-admin/KITTI")
+        validation_batch = reader.get_batch(params.BATCH_SIZE, validation_folders)
 
     deltas = batch[1]
     gammas = batch[2]
@@ -43,34 +48,35 @@ v_activations = net.get_activations(v_feature_map, 'valid_activations',reuse=Tru
 tf.summary.histogram('activations', activations)
 tf.summary.histogram('validation_activations', v_activations)
 
-with tf.name_scope('Losses'):
-    with tf.name_scope('deltas'):
-        d_loss = u.delta_loss(activations, deltas, mask,n_obj)
-        tf.summary.scalar('Delta_loss_training', d_loss)
-        v_d_loss = u.delta_loss(v_activations, v_deltas, v_mask, v_n_obj)
-        tf.summary.scalar('Delta_loss_validation', v_d_loss)
+with tf.device('/cpu:0'):
+    with tf.name_scope('Losses'):
+        with tf.name_scope('deltas'):
+            d_loss = u.delta_loss(activations, deltas, mask,n_obj)
+            tf.summary.scalar('Delta_loss_training', d_loss)
+            v_d_loss = u.delta_loss(v_activations, v_deltas, v_mask, v_n_obj)
+            tf.summary.scalar('Delta_loss_validation', v_d_loss)
 
-    with tf.name_scope('gammas'):
-        g_loss = u.gamma_loss(activations, gammas, mask, n_obj)
-        tf.summary.scalar('Gamma_loss_training', g_loss)
-        v_g_loss = u.gamma_loss(v_activations, v_gammas, v_mask, v_n_obj)
-        tf.summary.scalar('Gamma_loss_validation', v_g_loss)
+        with tf.name_scope('gammas'):
+            g_loss = u.gamma_loss(activations, gammas, mask, n_obj)
+            tf.summary.scalar('Gamma_loss_training', g_loss)
+            v_g_loss = u.gamma_loss(v_activations, v_gammas, v_mask, v_n_obj)
+            tf.summary.scalar('Gamma_loss_validation', v_g_loss)
 
-    with tf.name_scope('classes'):
-        c_loss = u.class_loss(activations, classes, mask, n_obj)
-        tf.summary.scalar('Class_loss_training', c_loss)
-        v_c_loss = u.class_loss(v_activations, v_classes, v_mask, v_n_obj)
-        tf.summary.scalar('Class_loss_validation', v_c_loss)
+        with tf.name_scope('classes'):
+            c_loss = u.class_loss(activations, classes, mask, n_obj)
+            tf.summary.scalar('Class_loss_training', c_loss)
+            v_c_loss = u.class_loss(v_activations, v_classes, v_mask, v_n_obj)
+            tf.summary.scalar('Class_loss_validation', v_c_loss)
 
-    loss = d_loss + g_loss + c_loss
-    tf.summary.scalar('Total_loss_training', loss)
-    v_loss = v_d_loss + v_g_loss + v_c_loss
-    tf.summary.scalar('Total_loss_validation', v_loss)
+        loss = d_loss + g_loss + c_loss
+        tf.summary.scalar('Total_loss_training', loss)
+        v_loss = v_d_loss + v_g_loss + v_c_loss
+        tf.summary.scalar('Total_loss_validation', v_loss)
 
 with tf.name_scope('Global_step'):
     global_step = tf.Variable(0, dtype=tf.int32)
 
-train_step = tf.train.AdamOptimizer(0.0001).minimize(loss, global_step=global_step)
+train_step = tf.train.AdamOptimizer(0.001).minimize(loss, global_step=global_step)
 
 merged = tf.summary.merge_all()
 
@@ -123,7 +129,8 @@ with sess.as_default():
     for i in range(200000):
         start_time=time.time()
         d, g, c, m, _t = sess.run([d_loss, g_loss, c_loss, merged, train_step])
-        print('training set took %f seconds!'%(time.time()-start_time))
+        t = time.time()-start_time
+        print('training set took %f seconds! (%s FPS)'%(t, params.BATCH_SIZE/t), end='\r')
         if (i%10 == 0):
             start_time=time.time()
             v_d, v_g, v_c, v_m = sess.run(
@@ -135,11 +142,12 @@ with sess.as_default():
 
             print("step %d, d_loss: %g, g_loss: %g, c_loss: %g" % (i, d, g, c))
             print("step %d, v_d_loss: %g, v_g_loss: %g, v_c_loss: %g" % (i, v_d, v_g, v_c))
-
+        if (i%1000 == 0):
+            start = time.time()
             saver.save(sess, folder_name + '/latest.cpt')
 
             #write graph to protobuf and then quantize
-            u.write_graph_to_pb(sess,\
+            '''u.write_graph_to_pb(sess,\
                 'activation/activations',\
                 'latest',\
                 folder_name)
@@ -153,8 +161,8 @@ with sess.as_default():
                 u.write_graph_to_pb(sess,\
                     'activation/activations',\
                     'best_valid',\
-                    folder_name)
-                print('saving took %f seconds!'%(time.time()-start_time))
+                    folder_name)'''
+            print('saving took %f seconds!'%(time.time()-start_time))
 
 
 writer.close()
